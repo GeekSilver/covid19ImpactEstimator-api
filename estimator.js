e6a1cb8ce7339b5588f96868ab76a3c1dc448fd7
GeekSilver <erickioko995@gmail.com>
2020-04-11 13:39:48 +0300

    update estimator function

--- estimator.js
+++ estimator.js
@@ -10,8 +10,22 @@ const path = require('path');
 const moneyLost = (infectedByRequestedTime, data) => {
   const income = data.region.avgDailyIncomeInUSD;
   const popRatio = data.region.avgDailyIncomePopulation;
-  const dollarsInFlight = infectedByRequestedTime * popRatio * income * data.timeToElapse;
-  return dollarsInFlight;
+  let dollarsInFlight;
+  let multiplier;
+  if (data.periodType === 'days') {
+    multiplier = infectedByRequestedTime * popRatio * income;
+    dollarsInFlight = multiplier / data.timeToElapse;
+  } else if (data.periodType === 'weeks') {
+    const time = data.timeToElapse * 7;
+    multiplier = infectedByRequestedTime * popRatio * income;
+    dollarsInFlight = multiplier / time;
+  } else if (data.periodType === 'months') {
+    const duration = data.timeToElapse * 30;
+    multiplier = infectedByRequestedTime * popRatio * income;
+    dollarsInFlight = multiplier / duration;
+  }
+
+  return Math.trunc(dollarsInFlight);
 };
 
 // time based
@@ -19,26 +33,27 @@ const casesByTime = (currentlyInfected, data) => {
   let cases;
   const duration = data.timeToElapse;
   if (data.periodType === 'days') {
-    cases = currentlyInfected * (2 ** Math.floor(duration / 3));
+    cases = currentlyInfected * (2 ** Math.trunc(duration / 3));
   } else if (data.periodType === 'weeks') {
-    cases = currentlyInfected * (2 ** Math.floor((duration * 7) / 3));
+    const time = duration * 7;
+    cases = currentlyInfected * (2 ** Math.trunc(time / 3));
   } else if (data.periodType === 'months') {
-    cases = currentlyInfected * (2 ** Math.floor((duration * 30) / 3));
+    cases = currentlyInfected * (2 ** (duration * 10));
   }
-  return cases;
+  return Math.trunc(cases);
 };
 
 // available beds in hospital
 const availableBedsByRequestedTime = (severeCasesByRequestedTime, data) => {
   let availableBeds;
 
-  const estAvailableBeds = Math.floor(0.35 * data.totalHospitalBeds);
+  const estAvailableBeds = 0.35 * data.totalHospitalBeds;
   if (estAvailableBeds < severeCasesByRequestedTime) {
     availableBeds = estAvailableBeds - severeCasesByRequestedTime;
   } else {
     availableBeds = estAvailableBeds;
   }
-  return availableBeds;
+  return Math.trunc(availableBeds);
 };
 
 // fn
@@ -56,18 +71,23 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.infectionsByRequestedTime = casesByTime(severeImpact.currentlyInfected, data);
 
   // those that require hospitalization
-  impact.severeCasesByRequestedTime = Math.floor(0.15 * impact.infectionsByRequestedTime);
-  severeImpact.severeCasesByRequestedTime = Math.floor(0.15 * severeImpact.infectionsByRequestedTime);
+  impact.severeCasesByRequestedTime = Math.trunc(0.15 * impact.infectionsByRequestedTime);
+  const svr1 = severeImpact.infectionsByRequestedTime;
+  severeImpact.severeCasesByRequestedTime = Math.trunc(0.15 * svr1);
 
   // 35% totalHospitalBeds are estimated to be available
-  impact.hospitalBedsByRequestedTime = availableBedsByRequestedTime(impact.severeCasesByRequestedTime, data);
-  severeImpact.hospitalBedsByRequestedTime = availableBedsByRequestedTime(severeImpact.severeCasesByRequestedTime, data);
+  const svr2 = impact.severeCasesByRequestedTime;
+  impact.hospitalBedsByRequestedTime = availableBedsByRequestedTime(svr2, data);
+  const impact1 = severeImpact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = availableBedsByRequestedTime(impact1, data);
   // 5% infectionsByRequestedTime are ICU cases
-  impact.casesForICUByRequestedTime = Math.floor(0.05 * impact.infectionsByRequestedTime);
-  severeImpact.casesForICUByRequestedTime = Math.floor(0.05 * severeImpact.infectionsByRequestedTime);
+  impact.casesForICUByRequestedTime = Math.trunc(0.05 * impact.infectionsByRequestedTime);
+  const impact2 = severeImpact.infectionsByRequestedTime;
+  severeImpact.casesForICUByRequestedTime = Math.trunc(0.05 * impact2);
   // 2% infectionsByRequestedTime require ventilators
-  impact.casesForVentilatorsByRequestedTime = Math.floor(0.02 * impact.infectionsByRequestedTime);
-  severeImpact.casesForVentilatorsByRequestedTime = Math.floor(0.02 * severeImpact.infectionsByRequestedTime);
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * impact.infectionsByRequestedTime);
+  const infct = severeImpact.infectionsByRequestedTime;
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infct);
   // amount of income lost due to illness
   impact.dollarsInFlight = moneyLost(impact.infectionsByRequestedTime, data);
   severeImpact.dollarsInFlight = moneyLost(severeImpact.infectionsByRequestedTime, data);
@@ -78,6 +98,7 @@ const covid19ImpactEstimator = (data) => {
     severeImpact,
   };
 };
+
 
 const controller = (req, res) => {
   let data;
